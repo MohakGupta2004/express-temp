fee27032d526a07f8d8e47c9a9730cb5765d5a52
Mohak Gupta <mohakgupta500@gmail.com>
2025-03-24 21:25:58 +0530

    Automated commit from API yo yo

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
-const express = require("express")
-const app = express()
-
-app.get("/", (req, res)=>{
-  res.send("WELCOME TO KGEC")
-})
-
-app.listen(5000, ()=>{
-  console.log("SERVER IS RUNNING")
-})
+console.log('Updated Content');
